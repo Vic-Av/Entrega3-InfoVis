ff90aaf4954875645f08073274aeac428aae5f9b
Vic-Av <vicenteac@estudiante.uc.cl>
2026-06-20 21:18:21 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+Protobject.setProduction(true)
+Protobject.initialize([
+  {
+    name: "Detector",
+    page: "detector.html",
+    debug: "local"
+  },
+  {
+    name: "Records",
+    page: "index.html",
+    main: true
+    debug: "master"
+  }
+]);
